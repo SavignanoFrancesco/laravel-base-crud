63776caffbe451ee0fff43f9630494f4ec271d42
Francesco <francesco.savignano96@gmail.com>
2021-01-20 17:53:26 +0100

    tabella grezza con le righe della tabella products impostata

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -3,6 +3,29 @@
 
 @section('content')
     <h1>Products</h1>
-    
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Gender</th>
+                    <th>Type</th>
+                    <th>Size</th>
+                    <th>Color</th>
+                    <th>Price</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($products as $product)
+                <tr>
+                    <td>{{$product->id}}</td>
+                    <td>{{$product->gender}}</td>
+                    <td>{{$product->type}}</td>
+                    <td>{{$product->size}}</td>
+                    <td>{{$product->color}}</td>
+                    <td>{{$product->price}}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
 
 @endsection
