7ea9c68d2fe4e6888bfda154a2c6054f030ac6c0
Francesco <francesco.savignano96@gmail.com>
2021-01-20 16:33:46 +0100

    migration per la tabella products impostata

---
+++ database/migrations/2021_01_20_152735_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('gender');
+            $table->string('type');
+            $table->string('size');
+            $table->string('color');
+            $table->float('price', 10, 2);
+            $table->text('dexcription')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
