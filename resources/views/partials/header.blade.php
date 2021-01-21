d77f874f94eb311c626a968feb7e57ec0ca62c50
Francesco <francesco.savignano96@gmail.com>
2021-01-21 16:08:48 +0100

    aggiunte directories per sass

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,12 +1,14 @@
 <nav>
     <div id="home-menu">
-        <ul>
-            <li class="link">
-                <a href="{{Route('home')}}">Home</a>
-            </li>
-            <li class="link">
-                <a href="{{route('products.index')}}">Products</a>
-            </li>
-        </ul>
+        <div class="container">
+            <ul>
+                <li class="link">
+                    <a href="{{Route('home')}}">Home</a>
+                </li>
+                <li class="link">
+                    <a href="{{route('products.index')}}">Products</a>
+                </li>
+            </ul>
+        </div>
     </div>
 </nav>
